116649d45a0c739170f69c094a60c8c160b5157d
Tejaswidarsi <122845794+Tejaswidarsi@users.noreply.github.com>
2026-04-02 19:05:37 +0530

    Update API import path in TrackStatus.js

--- front/src/pages/TrackStatus.js
+++ front/src/pages/TrackStatus.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import API from './api';
+import API from '../api';
 import { useNavigate, useParams } from 'react-router-dom';
 import './TrackStatus.css';
 
