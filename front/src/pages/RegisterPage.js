a7c6dcba7f40c1fc2caf8ee823c7bec003fa5785
Tejaswidarsi <122845794+Tejaswidarsi@users.noreply.github.com>
2026-04-02 19:04:51 +0530

    Fix import path for API in RegisterPage.js

--- front/src/pages/RegisterPage.js
+++ front/src/pages/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import API from './api';
+import API from '../api';
 import './student.css';
 
 const RegisterPage = () => {
