049133ab072ceea662cc3a309b24ebb70bdfeed9
Tejaswidarsi <122845794+Tejaswidarsi@users.noreply.github.com>
2026-04-02 19:05:54 +0530

    Change API import path in student-dashboard.js

--- front/src/pages/student-dashboard.js
+++ front/src/pages/student-dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import API from './api';
+import API from '../api';
 import './studentdashboard.css';
 
 const StudentDashboard = () => {
