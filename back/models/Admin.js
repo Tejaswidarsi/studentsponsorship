8b8ee34a6596bd029898225d45f12f83e1a162fd
Rahul Darsi <23JR1A0558@gmail.com>
2026-04-03 22:18:45 +0530

    fix: admin routes

--- back/models/Admin.js
+++ back/models/Admin.js
@@ -8,8 +8,13 @@ const adminSchema = new mongoose.Schema({
 
 adminSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error); // This passes the error to the 'catch' in your route
+  }
 });
 
 module.exports = mongoose.models.Admin || mongoose.model('Admin', adminSchema);
